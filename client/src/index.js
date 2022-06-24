a652e60473e67a2490e3eebffa086d0658cef6ca
LogXx <awesome149712@gmail.com>
2022-06-24 17:09:21 +0400

    Yay!

---
+++ client/src/index.js
@@ -0,0 +1,69 @@
+
+const React = require('react');
+const ReactDOM = require('react-dom/client');
+
+class Chat extends React.Component{
+	constructor(props){
+		super(props);
+		this.socket = new WebSocket('ws://' + window.location.hostname + ':7777');
+		this.state = {messages: []};
+		this.socket.onopen = () => this.appendMessage({author: 'SYSTEM', text: 'Connected.'});
+		this.socket.onmessage = (event) => {
+			const message = JSON.parse(event.data);
+			this.appendMessage(message);
+		}
+		this.socket.onclose = () => this.appendMessage({author: 'SYSTEM', text: 'Connection lost.'});
+	}
+	appendMessage(message){
+		this.setState(prevState => ({messages: prevState.messages.concat([message])}));
+	}
+	componentWillUnmount(){
+		this.socket.close();
+	}
+	render(){
+		return (<div>
+			{this.state.messages.map(({author, text}) => <Message author={author} text={text}/>)}
+			<TextField send={(text) => this.socket.send(text)}/>
+			</div>
+		);
+	}
+}
+
+function Message(props){
+	return (<div>
+		{props.author}: {props.text}
+		</div>);
+}
+
+class TextField extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {value: ''};
+		this.send = props.send;
+	}
+	handleChange(event){
+		this.setState({value: event.target.value});
+	}
+	handleSubmit(event){
+		event.preventDefault();
+		this.send(this.state.value);
+		this.setState({value: ''});
+	}
+	render(){
+		return (
+			<form onSubmit={(event) => this.handleSubmit(event)}>
+			<input type="text" value={this.state.value} onChange={(event) => this.handleChange(event)}/>
+			<button>Send</button>
+			</form>
+		);
+	}
+}
+
+function App(){
+	return (
+		<Chat/>
+	)
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
---
+++ server/index.js
@@ -0,0 +1,19 @@
+
+
+const ws = require('ws');
+const serv = new ws.WebSocketServer({port: 7777});
+
+const events = require('node:events');
+const chan = new events.EventEmitter();
+
+serv.on('connection', (sock) => {
+	const random_shit = Math.random().toString(36).slice(2, 7);
+	sock.on('message', (data) => {
+		chan.emit('data', {author: random_shit, text: data.toString()});
+	});
+	chan.on('data', (data) => {
+		sock.send(JSON.stringify(data));
+	});
+	sock.on('close', () => chan.emit('data', {author: 'SERVER', text: random_shit + ' left.'}));
+	chan.emit('data', {author: 'SERVER', text: random_shit + ' joined.'});
+});
